993858794ad089cbd5e9ed56310958755a33bf69
honeybhardwaj <honey.bhardwaj.18cse@bmu.edu.in>
2021-06-27 12:29:27 +0530

    added

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ from flask import Flask, redirect, url_for, render_template, request
 
 app=Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
+def about():
+    return render_template("about.html")
+
+@app.route("/bmi")
+def bmi():
     return render_template("bmi.html")
+
+@app.route("/edu")
+def edu():
+    return render_template("edu.html")
+
+@app.route("/cal")
+def cal():
+    return render_template("cal.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
