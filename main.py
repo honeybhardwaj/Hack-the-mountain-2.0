b398b7e33c1a0f5a6f2663f645f3e76a588fa3eb
honeybhardwaj <honey.bhardwaj.18cse@bmu.edu.in>
2021-06-27 11:25:22 +0530

    added

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("about.html")
+    return render_template("bmi.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
